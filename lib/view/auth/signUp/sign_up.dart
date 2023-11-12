b2d0ae7b3d23798231d3e5b2b6a29fe0e3283ad4
Amir55143 <aamirnoor55143@gmail.com>
2023-11-12 22:25:05 +0500

    SignUp

--- lib/view/auth/signUp/sign_up.dart
+++ lib/view/auth/signUp/sign_up.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:physio_doc/theme/style.dart';
 import 'package:physio_doc/view/auth/login/widgets/custom_textform.dart';
+import 'package:physio_doc/widgets/custom_button.dart';
 import 'package:physio_doc/widgets/custom_title.dart';
 
 class SignUp extends StatefulWidget {
@@ -13,39 +14,65 @@ class SignUp extends StatefulWidget {
 class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 40, vertical: 30),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 60),
-            CustomTitle(
-              title: 'Sign up', 
+        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 30),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 60),
+              const CustomTitle(
+                title: 'Sign up',
               ),
-            SizedBox(height: 30),  
-            CustomTextField(
-              hintText: 'first name',
-              hintColor: hintColor,
+              const SizedBox(height: 30),
+              const CustomTextField(
+                hintText: 'first name',
+                hintColor: hintColor,
               ),
-            SizedBox(height: 20),  
-            CustomTextField(
-              hintText: 'last name',
-              hintColor: hintColor,
-              ), 
-            SizedBox(height: 20),  
-            CustomTextField(
-              hintText: 'email',
-              hintColor: hintColor,
-              ), 
-            SizedBox(height: 20),  
-            CustomTextField(
-              hintText: 'password',
-              hintColor: hintColor,
-              ),   
-          ],
+              const SizedBox(height: 20),
+              const CustomTextField(
+                hintText: 'last name',
+                hintColor: hintColor,
+              ),
+              const SizedBox(height: 20),
+              const CustomTextField(
+                hintText: 'email',
+                hintColor: hintColor,
+              ),
+              const SizedBox(height: 20),
+              const CustomTextField(
+                hintText: 'password',
+                hintColor: hintColor,
+                isPassword: true,
+              ),
+              const SizedBox(height: 50),
+              CustomButton(
+                onTap: () {},
+                title: 'Signup',
+                color: buttonClr,
+              ),
+              const SizedBox(height: 40),
+              Column(
+                children: [
+                  const Center(
+                    child: Text(
+                      'OR',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w400,
+                        color: greyColor,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 40),
+                  Image.asset('assets/Group 16.png'),
+                ],
+              ),
+            ],
+          ),
         ),
-        ),
+      ),
     );
   }
 }
