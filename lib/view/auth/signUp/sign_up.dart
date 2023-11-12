c7829e914d3af588e6e11d75bdfa0c97938299dd
Amir55143 <aamirnoor55143@gmail.com>
2023-11-12 22:19:48 +0500

    TextForm

--- lib/view/auth/signUp/sign_up.dart
+++ lib/view/auth/signUp/sign_up.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:physio_doc/theme/style.dart';
+import 'package:physio_doc/view/auth/login/widgets/custom_textform.dart';
 import 'package:physio_doc/widgets/custom_title.dart';
 
 class SignUp extends StatefulWidget {
@@ -15,11 +17,32 @@ class _SignUpState extends State<SignUp> {
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 40, vertical: 30),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             SizedBox(height: 60),
             CustomTitle(
               title: 'Sign up', 
               ),
+            SizedBox(height: 30),  
+            CustomTextField(
+              hintText: 'first name',
+              hintColor: hintColor,
+              ),
+            SizedBox(height: 20),  
+            CustomTextField(
+              hintText: 'last name',
+              hintColor: hintColor,
+              ), 
+            SizedBox(height: 20),  
+            CustomTextField(
+              hintText: 'email',
+              hintColor: hintColor,
+              ), 
+            SizedBox(height: 20),  
+            CustomTextField(
+              hintText: 'password',
+              hintColor: hintColor,
+              ),   
           ],
         ),
         ),
