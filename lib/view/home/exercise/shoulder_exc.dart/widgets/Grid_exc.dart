3095d7e683e05832d17664adfcddd45a0c3d685c
Amir55143 <aamirnoor55143@gmail.com>
2023-11-15 14:56:12 +0500

    Image/Title

--- lib/view/home/exercise/shoulder_exc.dart/widgets/Grid_exc.dart
+++ lib/view/home/exercise/shoulder_exc.dart/widgets/Grid_exc.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class CustomGridExc extends StatefulWidget {
+  
   const CustomGridExc({super.key});
 
   @override
@@ -8,6 +9,101 @@ class CustomGridExc extends StatefulWidget {
 }
 
 class _CustomGridExcState extends State<CustomGridExc> {
+  final List<Map<String, dynamic>> gridMap = [
+    {
+      "title" : "Shoulder Girdle Depression Isometric Ex.",
+      "image" : "assets/Rctangle s1.png",
+    },
+    {
+      "title" : "Shoulder Girdle Depression Isometric Ex.",
+      "image" : "assets/Rctangle s2.png",
+    },
+    {
+      "title" : "Shoulder Girdle Depression Isometric Ex.",
+      "image" : "assets/Rctangle s3.png",
+    },
+    {
+      "title" : "Shoulder Girdle Depression Isometric Ex.",
+      "image" : "assets/Rctangle s4.png",
+    },
+    {
+      "title" : "Shoulder Girdle Depression Isometric Ex.",
+      "image" : "assets/Rctangle s5.png",
+    },
+    {
+      "title" : "Shoulder Girdle Depression Isometric Ex.",
+      "image" : "assets/Rctangle s6.png",
+    },
+    {
+      "title" : "Shoulder Girdle Depression Isometric Ex.",
+      "image" : "assets/Rctangle s7.png",
+    },
+    {
+      "title" : "Shoulder Girdle Depression Isometric Ex.",
+      "image" : "assets/Rctangle s8.png",
+    },
+    {
+      "title" : "Shoulder Girdle Depression Isometric Ex.",
+      "image" : "assets/Rctangle s9.png",
+    },
+    {
+      "title" : "Shoulder Girdle Depression Isometric Ex.",
+      "image" : "assets/Rctangle s10.png",
+    },
+    {
+      "title" : "Shoulder Girdle Depression Isometric Ex.",
+      "image" : "assets/Rctangle s11.png",
+    },
+    {
+      "title" : "Shoulder Girdle Depression Isometric Ex.",
+      "image" : "assets/Rctangle s12.png",
+    },
+    {
+      "title" : "Shoulder Girdle Depression Isometric Ex.",
+      "image" : "assets/Rctangle s13.png",
+    },
+    {
+      "title" : "Shoulder Girdle Depression Isometric Ex.",
+      "image" : "assets/Rctangle s14.png",
+    },
+    {
+      "title" : "Shoulder Girdle Depression Isometric Ex.",
+      "image" : "assets/Rctangle s15.png",
+    },
+    {
+      "title" : "Shoulder Girdle Depression Isometric Ex.",
+      "image" : "assets/Rctangle s16.png",
+    },
+    {
+      "title" : "Shoulder Girdle Depression Isometric Ex.",
+      "image" : "assets/Rctangle s17.png",
+    },
+    {
+      "title" : "Shoulder Girdle Depression Isometric Ex.",
+      "image" : "assets/Rctangle s18.png",
+    },
+    {
+      "title" : "Shoulder Girdle Depression Isometric Ex.",
+      "image" : "assets/Rctangle s19.png",
+    },
+    {
+      "title" : "Shoulder Girdle Depression Isometric Ex.",
+      "image" : "assets/Rctangle s20.png",
+    },
+    {
+      "title" : "Shoulder Girdle Depression Isometric Ex.",
+      "image" : "assets/Rctangle s21.png",
+    },
+    {
+      "title" : "Shoulder Girdle Depression Isometric Ex.",
+      "image" : "assets/Rctangle s22.png",
+    },
+    {
+      "title" : "Shoulder Girdle Depression Isometric Ex.",
+      "image" : "assets/Rctangle s23.png",
+    },
+
+  ];
   @override
   Widget build(BuildContext context) {
     return const Scaffold();
