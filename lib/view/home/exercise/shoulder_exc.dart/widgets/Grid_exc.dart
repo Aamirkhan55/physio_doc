a581b995ce6a9b8947a915fddb298506f845e0ae
Amir55143 <aamirnoor55143@gmail.com>
2023-11-15 12:56:14 +0500

    Grid

---
+++ lib/view/home/exercise/shoulder_exc.dart/widgets/Grid_exc.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CustomGridExc extends StatefulWidget {
+  const CustomGridExc({super.key});
+
+  @override
+  State<CustomGridExc> createState() => _CustomGridExcState();
+}
+
+class _CustomGridExcState extends State<CustomGridExc> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
